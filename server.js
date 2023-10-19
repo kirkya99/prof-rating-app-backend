55b02e175d5d4115cf74a121d0b94c49ed9c8a5c
kirkya99 <47646726+kirkya99@users.noreply.github.com>
2023-10-19 14:36:59 +0200

    Init backend finished

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const cors = require("cors");
+const port = 8080;
+const filename = __dirname + "/profs.json";
+
