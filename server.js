d8e19781f14fb83d060753275feb58b2362485cb
kirkya99 <fabianschotte@outlook.com>
2023-11-16 10:26:05 +0100

    Middleware

--- server.js
+++ server.js
@@ -5,3 +5,14 @@ const cors = require("cors");
 const port = 8080;
 const filename = __dirname + "/profs.json";
 
+// Middleware
+app.use(express.json()); // for parsing application/json
+app.use(cors()); // for configuring Cross-Origin Resource Sharing (CORS)
+function log(req, res, next) {
+    console.log(req.method + " Request at" + req.url)
+    next();
+}
+app.use(log);
+
+
+// Endpoints
