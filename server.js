6bcdb203163983c2292eb150a742e33fbc27dd40
kirkya99 <fabianschotte@outlook.com>
2023-11-18 15:51:32 +0100

    update

--- server.js
+++ server.js
@@ -34,34 +34,32 @@ async function main() {
 
 //Endpoints
 // Retrieve all saved entities
-app.get("/profs", function (req, res) {
+app.get("/profs", async function (req, res) {
     // TODO: Insert mongoDb query
-    res.send(JSON.stringify(retrieveEntries()));
-    res.status(200)
+    const query = {}
+    const result = await collection.find(query).toArray();
+    console.log(result);
+    res.status(200).send(result);
 });
 
 // Retrieve one saved entity
-app.get("/profs/:id", function (req, res) {
+app.get("/profs/:id", async function (req, res) {
     // TODO: Insert mongoDb query
-    retrieveEntry();
 
     res.status(200)
-
 });
 
 // Modify one entity
-app.put("/profs/:id", function (req, res) {
+app.put("/profs/:id", async function (req, res) {
     // TODO: Insert mongoDb query
-    modifyEntry();
 
     res.status(200)
 
 });
 
 // Delete one entity
-app.delete("/profs/:id", function (req, res) {
+app.delete("/profs/:id", async function (req, res) {
     // TODO: Insert mongoDb query
-    deleteEntry();
 
     res.status(200)
 
@@ -71,16 +69,15 @@ app.delete("/profs/:id", function (req, res) {
 app.post("/profs", async function (req, res) {
 
     // TODO: Insert mongoDb query    
-    const entry = {        
+    const entry = {
         name: req.body.name,
         rating: req.body.rating
     }
-    const result = await collection.insertOne(entry);
+    await collection.insertOne(entry);
 
-    // const allEntries = retrieveEntries();
-    res.status(200);
-    console.log(result);
-    res.send(JSON.stringify(result));
+    const query = {}
+    const queryResult = await collection.find(query).toArray();
+    res.status(200).send(queryResult);
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
