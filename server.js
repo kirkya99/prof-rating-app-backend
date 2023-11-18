f34e1f8faf0d8dc5a37ec602828562951d5d4063
kirkya99 <fabianschotte@outlook.com>
2023-11-18 12:24:18 +0100

    updated structure

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
 const cors = require("cors");
 const port = 8080;
-const filename = __dirname + "/profs.json";
+
+require('dotenv').config();
+
+const { MongoClient, ObjectId } = require('mongodb');
+
+const url = process.env.COSMOS_CONNECTION_STRING;
+const client = new MongoClient(url);
+
+// const string 
 
 //Middleware
 app.use(express.json()); //for parsing application/json
@@ -14,69 +21,40 @@ function log(req, res, next) {
 }
 app.use(log);
 
+connectToDatabase();
+
+async function connectToDatabase() {
+    // Use connect method to connect to the server
+    await client.connect();
+    // Database reference with creation if it does not already exist
+    const db = client.db(`prof-rating-app`);
+    console.log(`New database:\t${db.databaseName}\n`);
+}
 
 //Endpoints
+// Retrieve all saved entities
 app.get("/profs", function (req, res) {
-    fs.readFile(filename, "utf8", function (err, data) {
-        res.writeHead(200, {
-            "Content-Type": "application/json",
-        });
-        res.end(data);
-    });
+    // TODO: Insert mongoDb query
 });
 
+// Retrieve one saved entity
 app.get("/profs/:id", function (req, res) {
-    fs.readFile(filename, "utf8", function (err, data) {
-        const dataAsObject = JSON.parse(data)[req.params.id];
-        res.writeHead(200, {
-            "Content-Type": "application/json",
-        });
-        res.end(JSON.stringify(dataAsObject));
-    });
+    // TODO: Insert mongoDb query
 });
 
+// Modify one entity
 app.put("/profs/:id", function (req, res) {
-    fs.readFile(filename, "utf8", function (err, data) {
-        let dataAsObject = JSON.parse(data);
-        dataAsObject[req.params.id].name = req.body.name;
-        dataAsObject[req.params.id].rating = req.body.rating;
-        fs.writeFile(filename, JSON.stringify(dataAsObject), () => {
-            res.writeHead(200, {
-                "Content-Type": "application/json",
-            });
-            res.end(JSON.stringify(dataAsObject));
-        });
-    });
+    // TODO: Insert mongoDb query
 });
 
+// Delete one entity
 app.delete("/profs/:id", function (req, res) {
-    fs.readFile(filename, "utf8", function (err, data) {
-        let dataAsObject = JSON.parse(data);
-        dataAsObject.splice(req.params.id, 1);
-        fs.writeFile(filename, JSON.stringify(dataAsObject), () => {
-            res.writeHead(200, {
-                "Content-Type": "application/json",
-            });
-            res.end(JSON.stringify(dataAsObject));
-        });
-    });
+    // TODO: Insert mongoDb query
 });
 
+// Crrate a new entity
 app.post("/profs", function (req, res) {
-    fs.readFile(filename, "utf8", function (err, data) {
-        let dataAsObject = JSON.parse(data);
-        dataAsObject.push({
-            id: dataAsObject.length,
-            name: req.body.name,
-            rating: req.body.rating,
-        });
-        fs.writeFile(filename, JSON.stringify(dataAsObject), () => {
-            res.writeHead(200, {
-                "Content-Type": "application/json",
-            });
-            res.end(JSON.stringify(dataAsObject));
-        });
-    });
+    // TODO: Insert mongoDb query
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
